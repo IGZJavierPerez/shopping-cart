981d24e819561def48902afb2d3243359497318f
IGZJavierPerez <surtich@gmail.com>
2015-04-22 01:46:24 +0200

    Development Environment Setup

---
+++ README.md
@@ -0,0 +1,15 @@
+## React + flux App: Shopping Cart
+
+Based on [https://github.com/eggheadio/egghead-react-flux-example](egghead-react-flux-example)
+
+### To install
+
+```bash
+npm install
+```
+
+### To start
+
+```bash
+npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+/* gulpfile.js */
+
+// Load some modules which are installed through NPM.
+var gulp = require('gulp');
+var browserify = require('browserify');  // Bundles JS.
+var reactify = require('reactify');  // Transforms React JSX to JS.
+var source = require('vinyl-source-stream');
+var del = require('del');  // Deletes files.
+var util = require('gulp-util');
+var connect = require('gulp-connect');
+var runSequence = require('run-sequence');
+
+// Define some paths.
+var paths = {
+  main: ['./src/js/main.js'],
+  index: ['src/index.html'],
+  all_js: ['src/**/*.*'],
+  bower: ['src/bower_components/**/*']
+};
+
+// An example of a dependency task, it will be run before the css/js tasks.
+// Dependency tasks should call the callback to tell the parent task that
+// they're done.
+gulp.task('clean', function(done) {
+  del(['dist'], {force: true}, done);
+});
+
+//It will Browserify our code and compile React JSX files.
+gulp.task('browserify', function() {
+  // Browserify/bundle the JS.
+  browserify()
+  	.add(paths.main)
+    .transform(reactify)
+    .bundle()
+    .on('error', function(err){
+      util.log(util.colors.red('Error'), err.message);
+      this.emit('end');
+    })
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(connect.reload());
+});
+
+gulp.task('copy_index', function() {
+    gulp.src(paths.index)
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy_bower', function() {
+    gulp.src(paths.bower)
+    .pipe(gulp.dest('dist/bower_components'));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+});
+
+gulp.task('copy',['copy_index', 'copy_bower']);
+
+// Rerun tasks whenever a file changes.
+gulp.task('watch', function() {
+  gulp.watch(paths.all_js, ['dist']);
+});
+
+gulp.task('dist',['browserify', 'copy']);
+
+// The default task (called when we run `gulp` from cli)
+gulp.task('default', function(callback) {
+  runSequence('clean',
+              'dist',
+              ['connect', 'watch'],
+              callback);
+});
